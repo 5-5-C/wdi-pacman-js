e529b97eacc3bf607ac4757c5e2e69ee664740f6
5-5-C <jmarshall.brock@gmail.com>
2017-02-13 17:35:14 -0500

    added power pellet process and menu change and edible ghost reset

--- pacman.js
+++ pacman.js
@@ -1,7 +1,8 @@
 // Setup initial game stats
 var score = 0;
 var lives = 2;
-
+var powerPellets = 4;
+var hasPellets = true;
 
 // Define your ghosts here
 var Inky = {
@@ -46,7 +47,7 @@ function drawScreen() {
   clearScreen();
   setTimeout(function() {
     displayStats();
-    displayMenu();
+    displayMenu(hasPellets);
     displayPrompt();
   }, 10);
 }
@@ -56,10 +57,21 @@ function clearScreen() {
 }
 
 function displayStats() {
-  console.log('Score: ' + score + '     Lives: ' + lives);
+  console.log('Score: ' + score + '     Lives: ' + lives +'    PowerPellets: '+ powerPellets);
 }
 
-function displayMenu() {
+function displayMenu(hasPellets = true) {
+  if(hasPellets){
+  console.log('\n\nSelect Option:\n');  // each \n creates a new line
+  console.log('(d) Eat Dot');
+  console.log('(p) Eat Power Pellet');
+  console.log('(1) Eat Inky');
+  console.log('(2) Eat Blinky');
+  console.log('(3) Eat Pinky');
+  console.log('(4) Eat Clyde');
+  console.log('(q) Quit');
+}
+else {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
   console.log('(1) Eat Inky');
@@ -67,6 +79,7 @@ function displayMenu() {
   console.log('(3) Eat Pinky');
   console.log('(4) Eat Clyde');
   console.log('(q) Quit');
+}
 }
 
 function displayPrompt() {
@@ -102,14 +115,36 @@ function eatClyde() {
   score += 5;
 }
 
+function eatpowerPellet() {
+  checkPowerPellets();
+  console.log('\nChomp!');
+  score += 50;
+for (var i = 0; i < ghosts.length; i++) {
+  ghosts[i].edible = true;
+}
+powerPellets-- ;
+checkPowerPellets()
+}
+
+function checkPowerPellets() {
+  if (powerPellets == 0) {
+  console.log('\nYou have no Power Pellets Remaining');
+  hasPellets = false;
+  }
+  else {
+    hasPellets = true;
+  }
+}
+
 function eatGhosts(ghosts) {
   if (!ghosts.edible) {
     console.log('\nkilled by' + ghosts.name + ghosts.colour);
   lives--;
 }
 else {
-    console.log('\nAte' + ghosts.name);
-    score += 5;
+    console.log('\nYou Ate' + ghosts.name);
+    score += 200;
+    ghosts.edible = false;
   }
 }
 
@@ -132,6 +167,9 @@ function processInput(key) {
       break;
     case 'd':
       eatDot();
+      break;
+    case 'p':
+      eatpowerPellet();
       break;
     case '1':
       eatGhosts(ghosts[0]);
